e6fb39fd262351904417c7e375123a2790eec04e
Philip Bazzard <babarackas@gmail.com>
2016-12-05 14:33:03 -0800

    updating latest info from local computer

--- js/pizzacalc.js
+++ js/pizzacalc.js
@@ -1,22 +1,70 @@
-var input = document.getElementById('topping').value;
-
 var basicPizza = 10
 var myArr = {
-Gouda : 2,
-Cheddar : 3,
-Mozzarella : 1,
-Blue : 4,
-Feta : 2,
+plain : 0,
+gouda : 2,
+cheddar : 3,
+mozzarella : 1,
+blue : 4,
+feta : 2,
 }
-var total = 0
+
+function totalPriceOutput(){
+    var initVal = document.getElementById('myTextField').value;
+    var lowercase = initVal.toLowerCase('myTextField');
+    var output = document.getElementById('toppings');
+    var array = lowercase.split(',');
+   
+var total = 0;
 var i;
-for (i=0; i<myArr.length; i++) {
-total = (+total) + (+input[myArr[1]]);
+
+for (i=0; i<array.length; i++) {
+array[i] = array[i].trim();
+total = (+total) + (+myArr[array[i]]);
+totalPrice = "Toppings Selected --- " + lowercase + " --- Total = $" + (total + basicPizza);
+}
+    
+    output.innerHTML = "$" + (total + basicPizza);    
 }
 
-var totalPrice = total + basicPizza;
-
-function thanks() {
-var a = document.getElementById('topping');
-alert("Thanks for ordering! Your total cost is $"+ totalPrice);
+function myModalChange() {
+document.getElementById("myVar").innerHTML = "Thanks for ordering! " + totalPrice;
 }
+var locations = [
+    [
+        "Tuscany Village Location 1 Km to Uvic",
+        48.468868, 
+        -123.331232
+    ],
+    [
+    		"Fishermans Warf Location 9.1 Km to Uvic",
+        48.422738, -123.383144
+    ],
+    [
+    		"Mayfare Mall Location 5.2 Km to Uvic",
+        48.447658, -123.369150
+    ]
+]
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 12,
+      center: new google.maps.LatLng(48.440911, -123.350674),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+ var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
